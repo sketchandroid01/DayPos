b986c58e981d59b8b8d48f9006229e35a2516ace
developer <sketch.dev01@gmail.com>
2020-01-23 20:03:21 +0530

    commit-sourav

--- app/src/main/java/com/daypos/fragments/customers/Customers.java
+++ app/src/main/java/com/daypos/fragments/customers/Customers.java
@@ -1,4 +1,53 @@
 package com.daypos.fragments.customers;
 
-public class Customers {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.daypos.R;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class Customers extends Fragment {
+
+    private Unbinder unbinder;
+
+
+
+
+    public Customers() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_customer, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        viewsAction();
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+
+    private void viewsAction(){
+
+
+
+
+    }
+
+
+
 }
