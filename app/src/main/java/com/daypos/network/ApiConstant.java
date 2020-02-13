73a7ae622b0154b233b9fe4a9ee9782922041d7d
developer <sketch.dev01@gmail.com>
2020-02-13 18:23:38 +0530

    commit-souravOffice

--- app/src/main/java/com/daypos/network/ApiConstant.java
+++ app/src/main/java/com/daypos/network/ApiConstant.java
@@ -4,10 +4,10 @@ public class ApiConstant {
 
 
     /// lab domain
-    private static final String DOMAIN = "https://lab-5.sketchdemos.com/PHP-WEB-SERVICES/P-1029-DAYPOS/";
+   // private static final String DOMAIN = "https://lab-5.sketchdemos.com/PHP-WEB-SERVICES/P-1029-DAYPOS/";
 
     ///
-   // private static final String DOMAIN = "https://teq-dev-var19.co.in/daypos/";
+    private static final String DOMAIN = "https://teq-dev-var19.co.in/daypos/";
 
     private static final String BASE_URL = DOMAIN + "Api/";
 
@@ -25,9 +25,9 @@ public class ApiConstant {
     public static final String search_item_list              = BASE_URL + "search_item_list";
     public static final String add_to_cart                   = BASE_URL + "add_to_cart";
     public static final String cart_item_list                = BASE_URL + "cart_item_list";
-    public static final String cart_item_delete                = BASE_URL + "cart_item_delete";
-    public static final String cart_item_quantity_update                = BASE_URL + "cart_item_quantity_update";
-    public static final String customer_list_by_userID                = BASE_URL + "customer_list_by_userID";
+    public static final String cart_item_delete              = BASE_URL + "cart_item_delete";
+    public static final String cart_item_quantity_update     = BASE_URL + "cart_item_quantity_update";
+    public static final String customer_list_by_userID       = BASE_URL + "customer_list_by_userID";
 
 
 
